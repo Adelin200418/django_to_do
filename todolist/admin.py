ae9ff4dc3ef1e6072ff532bc2e5269b6ef0cf044
Adelin <adelinvoicu08@gmail.com>
2021-08-15 23:56:48 +0300

    new commit

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from .models import Todolist
 
 # Register your models here.
--- todolist/forms.py
+++ todolist/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 
-class TodoListForm(forms.Form):
-    text = forms.CharField(max_length=45,
+class  TodoListForm(forms.Form):
+    text= forms.CharField(max_length=45,
         widget=forms.TextInput(
-            attrs={'class' : 'form-control', 'placeholder' :'Enter todo e.g Grocery Shopping', 'aria-label' :'Todo', 'aria-describeby' : 'add-btn'}))
-
+            attrs={'class':'form-control','placeholder':'Enter to do e.g wash dishes','aria-label':'Todo','aris-describeby':'add-btn'}
+        ))
